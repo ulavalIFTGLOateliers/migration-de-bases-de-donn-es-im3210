c5f150d1e9b30d5a79f585147131c17bd764de57
imbou29 <imen.boukhit.1@ulaval.ca>
2024-03-31 06:18:34 -0400

    commit

--- db_scripts/migrate_1.sql
+++ db_scripts/migrate_1.sql
@@ -1,21 +1,42 @@
 ALTER TABLE singer RENAME TO musician;
 
 ALTER TABLE musician RENAME COLUMN singerName TO musicianName;
-ALTER TABLE musician ADD COLUMN role varchar (50);
-ALTER TABLE musician ADD COLUMN bandName varchar (50);
+ALTER TABLE musician ADD role varchar(50);
+ALTER TABLE musician ADD bandName varchar(50);
 
 
-CREATE TABLE IF NOT EXISTS band(
-     bandName varchar (50) PRIMARY KEY,
-     creation YEAR, 
-     genre varchar (50));
 
-INSERT INTO band (bandName, creation, genre) VALUES 
-                                                  ('crazy Duo', 2015, 'rock'), 
-                                                  ('Luna',2009,'classical'),
-                                                  ('Mysterio',2019,'pop');
 
-UPDATE musician SET role = 'Vocals', bandName ='Crazy Duo ' WHERE musicianName = 'Alina';
-UPDATE musician SET role = 'Guitar', bandName = 'Mysterio' WHERE musicianName = 'Mysterio';
-UPDATE musician SET role ='percussion', bandName ='Crazy Duo' WHERE musicianName ='Rainbow';
-UPDATE musician SET role = 'piano', bandName= 'Luna' WHERE musicianName = 'Luna';
+CREATE TABLE IF NOT EXISTS band (
+    bandName VARCHAR(50) PRIMARY KEY,
+    creation YEAR,
+    genre VARCHAR(50)
+);
+
+INSERT INTO band (bandName, creation, genre) VALUES
+                                                 ('Crazy Duo', 2015, 'rock'),
+                                                 ('Luna', 2009, 'classical'),
+                                                 ('Mysterio',2019, 'pop');
+
+
+
+
+UPDATE musician SET role = 'vocals',
+                    bandName = 'Crazy Duo'
+                        WHERE musicianName = 'Alina';
+
+
+
+UPDATE musician SET role = 'guitar',
+                    bandName = 'Mysterio'
+                        WHERE musicianName = 'Mysterio';
+
+
+UPDATE musician SET role = 'percussion',
+                    bandName = 'Crazy Duo'
+                        WHERE musicianName = 'Rainbow';
+
+
+UPDATE musician SET role = 'piano',
+                    bandName = 'Luna'
+                        WHERE musicianName = 'Luna';
